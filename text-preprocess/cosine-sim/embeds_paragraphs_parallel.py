29792a632fdd26f8011b9df8ab12423d2d70f654
Romain Lacombe <romain.lacombe@gmail.com>
2023-03-16 00:43:05 +0100

    Clean up parallel paragraph embeddings

--- text-preprocess/cosine-sim/embeds_paragraphs_parallel.py
+++ text-preprocess/cosine-sim/embeds_paragraphs_parallel.py
@@ -14,8 +14,6 @@ dir_path= args.directory
 files = os.listdir(dir_path)
 num_files = len(files)
 
-embeds = pd.DataFrame(columns=["cid", "para_num", "para_embed"])
-
 # Load pre-trained BERT model and tokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained('bert-base-uncased')
@@ -24,15 +22,12 @@ max_bert_token_length = 512
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Using device:', device)
 
-tokenizer
 model.to(device)
 
 # Loop through each text file in the directory
 for i, filename in enumerate(files):
 
     if filename.endswith(".txt"):
-
-        embeds = pd.DataFrame(columns=["cid", "para_num", "para_embed"])   
 
         # Get molecule CID
         cid = int(filename.split("_")[1].split(".")[0])
@@ -57,16 +52,13 @@ for i, filename in enumerate(files):
                     continue
 
                 # Convert tokens to tensor  
-                paragraph_tensor = torch.tensor([tokenizer.convert_tokens_to_ids(paragraph_tokens)]).long()
-                #print(paragraph_tensor.shape)
+                paragraph_tensor = torch.tensor([tokenizer.convert_tokens_to_ids(paragraph_tokens)]).short()
                 paragraph_tensors_list.append(paragraph_tensor.T)                
                 print(f"Done with paragraph {l}.")
 
             # Pad and stack tensors along the batch dimension
             paragraph_tensors = pad_sequence(paragraph_tensors_list, batch_first=True).squeeze(2).to(device)
-            print(paragraph_tensors.shape)
-            #paragraph_tensors = torch.stack(padded_tensors, dim=0).to(device)
-
+            
             # Get BERT embeddings for full tensor
             with torch.no_grad():
                 paragraph_embeddings = model(paragraph_tensors)[0][:, 0, :].to(device)
