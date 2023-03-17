96813fec01e131cdf96ca2b44839558b0e6c3711
AndrewJGaut <ajg@umail.ucsb.edu>
2023-03-17 15:44:12 -0700

    add in preproc script and make max token length 512

---
+++ text-preprocess/manual_evaluation_preprocess.py
@@ -0,0 +1,21 @@
+
+
+
+def process_file(infile_path):
+  outfile_path = "processed.txt"
+  outfile_str = list()
+  with open(infile_path, 'r') as f:
+    for i, para in enumerate(f.readlines()):
+      para = para[:256] 
+      outfile_str.append("-"*80)
+      outfile_str.append("i: {} {}".format(i, para))
+  outfile_str = "\n".join(outfile_str)
+  f = open(outfile_path, 'w')
+  f.write(outfile_str)
+
+if __name__ == '__main__':
+  process_file('/Users/agaut/Downloads/text_502.txt')
+
+
+
+
