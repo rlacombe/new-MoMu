c547878ab98f853527cfc8ef9a39fb038ea253c0
Romain Lacombe <romain.lacombe@gmail.com>
2023-03-19 03:48:37 +0100

    id: use non-augmented molecule as one sample

--- new-contrast/data_provider/pretrain_dataset.py
+++ new-contrast/data_provider/pretrain_dataset.py
@@ -112,7 +112,7 @@ class GINPretrainDataset(Dataset):
         # sl = torch.tensor([[n, n] for n in range(node_num)]).t()
         # data.edge_index = torch.cat((data.edge_index, sl), dim=1)
 
-        if graph_aug = 'id':
+        if graph_aug == 'id':
             data_aug = deepcopy(data)
         elif graph_aug == 'chem':
             data_aug = methylation(deepcopy(data))        
